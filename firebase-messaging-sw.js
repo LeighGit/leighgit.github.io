223d9be91dad4b2c03a0e1ea8f6b1000db5dd0cc
LeighGit <40258610+LeighGit@users.noreply.github.com>
2018-06-20 10:18:25 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,6 @@
 // Import and configure the Firebase SDK
-// These scripts are made available when the app is served or deployed on Firebase Hosting
-// If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
 importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.4/firebase.js');
 
 // Initialize Firebase
 	var config = {
